f35310079209a0a06f3f416e8795db7425a804c6
brahim0 <brahimhm470@gmail.com>
2018-08-23 00:52:41 +0100

    product resource2

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,12 @@ class ProductResource extends JsonResource
              'name' => $this->name,
              'description' => $this->detail,
              'price' => $this->price,
-             'stock' => $this->stock,
-             'discount' => $this->discount
+             'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+             'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+             'totalPrice' => round( $this->price- ($this->price*($this->discount/100)),2),
+             'href' => [
+               'reviews' => route('reviews.index',$this->id),
+             ],
         ];
     }
 }
